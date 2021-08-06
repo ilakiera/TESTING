488bc6d8ce46bc7b2eb9576bf9a64b364d1a5535
Ila Nor Syakira <44170201+ilakiera@users.noreply.github.com>
2021-08-06 16:26:44 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<title>Site Redirect</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px;}
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+        <img src="mycitelogo.png" alt="Italian Trulli">
+    <h1>Harap Maaf</h1>
+    <div>
+        <p>Laman sesawang ini sedang diselenggara</p>
+        <p>Sebarang pertanyaan boleh emel kepada:</p>
+        <a href="mailto:mcc@mohe.gov.my">mcc@mohe.gov.my</a>
+    </div>
+</article>
+
+
+
+
+
